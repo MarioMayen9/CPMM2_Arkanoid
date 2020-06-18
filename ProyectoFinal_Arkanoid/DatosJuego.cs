7d14e12cd256fbc442f0c6a137c1774bf5160d75
Carlosuca <55195969+Carlosuca@users.noreply.github.com>
2020-06-17 18:29:09 -0600

    Merge branch 'master' of C:\Users\Carlos\Desktop\Proyecto\CPMM2_Arkanoid with conflicts.

--- ProyectoFinal_Arkanoid/DatosJuego.cs
+++ ProyectoFinal_Arkanoid/DatosJuego.cs
@@ -3,6 +3,11 @@
     public class DatosJuego
     {
         public static bool juegoIniciado = false;
-        public static int dirX = 7, dirY = -dirX;
+        public static int dirX = 7, dirY = -dirX, vidas = 3, puntaje = 0;
+
+        public static void inicializarJuego()
+        {
+            
+        }
     }
 }
